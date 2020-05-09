987ad168dfb6ef04fedd055a216942c12b0fce99
anphungithcmus <anphung1681997@gmail.com>
2020-05-09 15:25:12 +0700

    Update multi-task

--- skype_bot.py
+++ skype_bot.py
@@ -29,8 +29,10 @@ async def main():
 	ch=sk.chats
 
 	#Call back to handle message in all groups
-	for group_link in skypeGroup_arr[1:]:
-		await asyncio.gather(async_group_message(client, ch, group_link))
+	while True:
+		for group_link in skypeGroup_arr[1:]:
+			await async_group_message(client, ch, group_link)
+		time.sleep(5)
 
 def update_task(userID, task, worksheet):
 	# Find user
@@ -56,30 +58,64 @@ async def async_group_message(client, ch, group_link):
 			if j.time >= fiveminute:    # in last 5 minute
 				if j.content.find('#') == 0:  #task
 					print(j.content)
-					start = j.content.find('id="8:') + 6
-					end = j.content.find('">')
-					userID = j.content[start:end] # Find user id
-					start = j.content.find('</at>') + 6
-					task = j.content[start:] + ' ' + str(j.time)# Find task
-					print(userID)
-					print(task)
-					# Find a workbook and open sheet by name
+					tasks = fillter_task(j)
+					#Find a workbook and open sheet by name
 					dt = datetime.datetime.today()
 					sheet_name = str(dt.day) + '/' + str(dt.month)
 					worksheet = client.open("Tempo 5000").worksheet(sheet_name)
-					update_task(userID, task, worksheet)
-					alert_skype(j.user.name, userID, task, ch)
-					print('Sent task to: ',userID)
+					for task in tasks:
+						update_task(task['userID'], task['task_content'], worksheet)
+						alert_skype(j.user.name, task['userID'], task['task_content'], ch, True)
+						print('Sent task to: ', task['userID'])
 				else:
 					continue
-		time.sleep(5)
+		break
 
-def alert_skype(LeaderName,UserID,Task, ch):
-	idSkype='8:'+UserID
+def fillter_task(j):
+	task_total = []
+	task_key='# <at id="8:'
+	hashtag_key = '<at id="8:'
+	content = j.content
+	while True:
+		if content.find(task_key) != -1:
+			start = content.find(task_key) + len(task_key)
+			end = content.find('">')
+			userID = content[start:end] # Find user id
+			content = content[end + 2:]	# Update content
+			start = content.find('</at>') + 5 # Find task content of UserID above
+			print(content)
+			print(content.find(hashtag_key))
+			if content.find(hashtag_key) != -1:
+				end = content.find(hashtag_key)
+				task_content = content[start:end] + ' ' + str(j.time)# Find task
+				new_task = {
+					"userID" : userID,
+					"task_content" : task_content
+				}
+				task_total.append(new_task)
+				content = content[end:]
+				task_key = hashtag_key #Update key
+			else:
+				task_content = content[start:] + ' ' + str(j.time)# Find task
+				new_task = {
+					"userID" : userID,
+					"task_content" : task_content
+				}
+				task_total.append(new_task)
+				content=''
+		else:
+			return task_total
+
+def alert_skype(leaderName,userID,Task, ch, status, user=None, time=None, content=None):
+	idSkype='8:'+userID
 	ch1= ch.chat(idSkype) # connect to chat using id
 	now =datetime.datetime.now()
-	alert = 'You have a new Task from ' + SkypeMsg.bold(str(LeaderName)) + chr(10) + SkypeMsg.bold(str(Task))
-	ch1.sendMsg(alert, rich=True)
+	if status == True:
+		alert = 'You have a new Task from ' + SkypeMsg.bold(str(leaderName)) + chr(10) + SkypeMsg.bold(str(Task))
+		ch1.sendMsg(alert, rich=True)
+	else:
+		alert = SkypeMsg.quote(user, ch1, time1,content1)  + chr(10) + 'syntax error'
+		ch1.sendMsg(alert, rich=True)
 
 if __name__ == '__main__':
 	asyncio.run(main())
